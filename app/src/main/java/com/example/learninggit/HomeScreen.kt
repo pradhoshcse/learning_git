34ebf13cf40f72be01f67d4da1daec84a58dd59c
Pradhosh_S-trimble-6jNTys <pradhosh_ss@trimble.com>
2024-12-31 11:28:54 +0530

    Developed HomeScreen

---
+++ app/src/main/java/com/example/learninggit/HomeScreen.kt
@@ -0,0 +1,21 @@
+package com.example.learninggit
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun HomeScreen() {
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Text(text = "Home Screen",
+            fontSize = 32.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(20.dp))
+    }
+}
